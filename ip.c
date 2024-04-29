afd6691422918ad49f7968138394d4e475350aca
Tatsuya Murakami <msgtatsuya@gmail.com>
2024-04-29 20:01:01 +0900

    Done day2 step7.

--- ip.c
+++ ip.c
@@ -3,6 +3,8 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+#include "platform.h"
+
 #include "util.h"
 #include "net.h"
 #include "ip.h"
@@ -23,6 +25,9 @@ struct ip_hdr {
 
 const ip_addr_t IP_ADDR_ANY       = 0x00000000;
 const ip_addr_t IP_ADDR_BROADCAST = 0xffffffff;
+
+
+static struct ip_iface *ifaces;
 
 int
 ip_addr_pton(const char *p, ip_addr_t *n) // Printable text to Network binary
@@ -90,12 +95,79 @@ ip_dump(const uint8_t *data, size_t len)
     funlockfile(stderr);
 }
 
+struct ip_iface *
+ip_iface_alloc(const char *unicast, const char *netmask)
+{
+    struct ip_iface *iface;
+
+    iface = memory_alloc(sizeof(*iface));
+    if (!iface) {
+        errorf("memory_alloc() failure.");
+        return NULL;
+    }
+    NET_IFACE(iface)->family = NET_IFACE_FAMILY_IP;
+
+    // Exercise 7-3
+    if (ip_addr_pton(unicast, &iface->unicast) == -1) {
+        errorf("ip_addr_pton(unicast) failure.");
+        memory_free(iface);
+        return NULL;
+    }
+
+    if (ip_addr_pton(netmask, &iface->netmask) == -1) {
+        errorf("ip_addr_pton(netmask) failure.");
+        memory_free(iface);
+        return NULL;
+    }
+
+    iface->broadcast = (iface->unicast & iface->netmask) | (~iface->netmask);
+
+    return iface;
+}
+
+
+int
+ip_iface_register(struct net_device *dev, struct ip_iface *iface)
+{
+    char addr1[IP_ADDR_STR_LEN];
+    char addr2[IP_ADDR_STR_LEN];
+    char addr3[IP_ADDR_STR_LEN];
+
+    // Exercise 7-4
+    if (net_device_add_iface(dev, (struct net_iface *)iface) == -1) {
+        errorf("net_device_add_iface() error.");
+        return -1;
+    }
+    iface->next = ifaces;
+    ifaces = iface;
+
+    infof("registered: dev=%s, unicast=%s, netmask=%s, broadcast=%s", dev->name,
+        ip_addr_ntop(iface->unicast, addr1, sizeof(addr1)),
+        ip_addr_ntop(iface->netmask, addr2, sizeof(addr2)),
+        ip_addr_ntop(iface->broadcast, addr3, sizeof(addr3)));
+    return 0;
+}
+
+struct ip_iface *
+ip_iface_select(ip_addr_t addr)
+{
+    struct ip_iface *entry;
+    for (entry = ifaces; entry; entry = entry->next) {
+        if (entry->unicast == addr) {
+            break;
+        }
+    }
+    return entry;
+}
+
 static void
 ip_input(const uint8_t *data, size_t len, struct net_device *dev)
 {
     struct ip_hdr *hdr;
     uint8_t v;
     uint16_t hlen, total, offset;
+    struct ip_iface *iface;
+    char addr[IP_ADDR_STR_LEN];
 
     if (len < IP_HDR_SIZE_MIN) {
         errorf("too short");
@@ -104,17 +176,6 @@ ip_input(const uint8_t *data, size_t len, struct net_device *dev)
     hdr = (struct ip_hdr *)data;
 
     // Exercixe 6-1
-    /*
-    Exercise 6-1: IPデータグラムの検証
-    (1) バージョン
-        IP_VERSION_IPV4 と一致しない場合はエラーメッセージを出力して中断
-    (2) ヘッダ長
-        入力データの長さ（len）がヘッダ長より小さい場合はエラーメッセージを出力して中断
-    (3) トータル長
-        入力データの長さ（len）がトータル長より小さい場合はエラーメッセージを出力して中断
-    (4) チェックサム
-        cksum16() での検証に失敗した場合はエラーメッセージを出力して中断
-    */
     v = (hdr->vhl & 0xf0) >> 4;
     if (v != IP_VERSION_IPV4) {
         errorf("unsupported protocol version v:%u", v);
@@ -143,7 +204,24 @@ ip_input(const uint8_t *data, size_t len, struct net_device *dev)
         errorf("fragments does not support.");
         return;
     }
-    debugf("dev=%s, protocol=%u, total=%u", dev->name, hdr->protocol, total);
+
+    // Exercise 7-6
+    iface = (struct ip_iface *)net_device_get_iface(dev, NET_IFACE_FAMILY_IP);
+
+    if (!iface) {
+        return;
+    }
+
+    if (
+        (iface->unicast != hdr->dst) &&
+        (IP_ADDR_BROADCAST != hdr->dst) &&
+        (iface->broadcast != hdr->dst)
+    ) {
+        return;
+    }
+
+    debugf("dev=%s, iface=%s, protocol=%u, total=%u",
+        dev->name, ip_addr_ntop(iface->unicast, addr, sizeof(addr)), hdr->protocol, total);
     ip_dump(data, total);
 }
 
